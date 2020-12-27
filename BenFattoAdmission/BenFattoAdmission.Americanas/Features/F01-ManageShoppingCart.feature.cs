6d66081e81eb63eb1d2df7161cc5310fdfa61fb9
Thiago S Galdino <thiago.secomandi@savecash.com.br>
2020-12-27 07:54:13 -0300

    Renaming solution and project

--- BenFattoAdmission/BenFattoAdmission.Americanas/Features/F01-ManageShoppingCart.feature.cs
+++ BenFattoAdmission/BenFattoAdmission.Americanas/Features/F01-ManageShoppingCart.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace BenFattoAdmission.Americanas.Features
+namespace Americanas.ShoppingCart.Features
 {
     using TechTalk.SpecFlow;
     using System;
